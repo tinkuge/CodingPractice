b7a6057522b2b406dad7286eafaf9c105083b3ed
tinkuge <dinakarge@gmail.com>
2020-09-09 11:02:44 -0700

    More examples

---
+++ SimStrings.java
@@ -0,0 +1,9 @@
+public class SimStrings {
+    public static void main(String[] args) {
+        checkSim(String s1, String s2);
+    }
+
+    static void checkSim(String s, String t){
+        
+    }
+}
